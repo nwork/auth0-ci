4d4b48c731467d7423e08e080dcf5b1820a18245
Guillaume Destuynder <kang+mozilla@insecure.ws>
2018-06-29 10:52:33 -0700

    bump to 1.0.4

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup
 
 setup(
     name="Auth0-ci",
-    version="1.0.3",
+    version="1.0.4",
     author="Guillaume Destuynder",
     author_email="gdestuynder@mozilla.com",
     py_modules=["uploader_login_page", "uploader_rules", "uploader_clients"],
